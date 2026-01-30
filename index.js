517fa7cab560eb66af18b5211309ecc97961a67f
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-30 09:31:12 -0600

    Refactor add function to perform addition

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let add = function (a*b) =>{
-  return a*b
+let add => function (a+b) =>{
+  return a+b
 };
 
-document.getElementById("result").innerHTML = add (a*b);
+document.getElementById("result").innerHTML = add (a+b);
+
 
 
